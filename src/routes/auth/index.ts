495a548638ef24e47913f9c9430f8722177341be
mclainv <rma9540@nyu.edu>
2025-04-18 01:20:33 -0400

    added session store through mongo

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import passport from "passport";
+
 const router = Router();
 
 router.get('/discord', passport.authenticate('discord'), (req, res) => {
     res.send(200);
+    return;
 });
+
 router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => {
     res.send({msg: 'Success'});
+    return;
+});
+
+router.get('/status', (req, res) => {
+    if (req.user) {
+        res.send(req.user);
+    } else {
+        res.status(401).send({
+            msg: "Unauthorized"
+        });
+    }
+    return;
 });
 
 export default router;
