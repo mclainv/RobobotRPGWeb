181e5c99153ac63996051d9d03df45cb839027c2
mclainv <rma9540@nyu.edu>
2025-04-29 23:40:10 -0400

    added forced node version and changed run.sh because not everything was running

--- run.sh
+++ run.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
+# cd "$(dirname "$0")" is this needed?
 set -e
 
-echo "Setting up backend..."
+# Install both backend and frontend dependencies
 cd RobobotRPG-api
+npm install
+cd ../robobotrpgweb-client
 npm install
 cd ..
 
-echo "Starting backend in background..."
+# Start both backend and frontend in parallel
 cd RobobotRPG-api
 npm run dev &
-cd ..
-
-echo "Setting up frontend..."
-cd robobotrpgweb-client
-npm install
-npm run dev
-
-# End of script; background processes will continue running
+cd ../robobotrpgweb-client
+npm run dev &
