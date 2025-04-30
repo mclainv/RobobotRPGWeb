b1c17c87d418e5a543e78907fdd32d0188045da3
mclainv <rma9540@nyu.edu>
2025-04-30 03:15:06 -0400

    fixed origin issue in client api

--- robobotrpgweb-client/src/utils/api.ts
+++ robobotrpgweb-client/src/utils/api.ts
@@ -2,16 +2,18 @@ import { GetServerSidePropsContext } from "next";
 import axios from 'axios';
 import { validateCookies } from "./helpers";
 import { PartialGuild } from "./types";
+import absoluteUrl from 'next-absolute-url';
 // import '../config';
 
 export const fetchMutualGuilds = async (context: GetServerSidePropsContext) => {
     const headers = validateCookies(context);
     console.log("headers are ", headers);
     if (!headers) return { redirect: { destination: '/'} };
+    // Derive full origin for server-side requests
+    const { origin } = absoluteUrl(context.req);
     try {
-        const { data: guilds } = await axios.get<PartialGuild[]>('/api/guilds', {
-            headers,
-            withCredentials: true,
+        const { data: guilds } = await axios.get<PartialGuild[]>(`${origin}/api/guilds`, {
+            headers
         });
         console.log("guilds are ", guilds);
         return { props: { guilds } } ;
@@ -21,10 +23,13 @@ export const fetchMutualGuilds = async (context: GetServerSidePropsContext) => {
     }
 }
 
-export const fetchValidGuild = async (guildId: string, headers: HeadersInit) => {
-    const response = await fetch(`/api/guilds/${guildId}/permissions`, {
-        headers,
-        credentials: 'include',
+export const fetchValidGuild = async (context: GetServerSidePropsContext, guildId: string) => {
+    const headers = validateCookies(context);
+    if (!headers) return { redirect: { destination: '/'} };
+    const { origin } = absoluteUrl(context.req);
+    // Client-side call remains relative
+    const response = await fetch(`${origin}/api/guilds/${guildId}/permissions`, {
+        headers
     });
     return response;
 }
