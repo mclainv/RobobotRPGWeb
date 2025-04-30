801db8090948fec521925321e23fd4d0169859e8
mclainv <rma9540@nyu.edu>
2025-04-29 22:53:40 -0400

    changed devcontainer

---
+++ run.sh
@@ -0,0 +1,17 @@
+ #!/bin/sh
+ set -e  # Exit if any command fails
+
+ echo "Setting up frontend..."
+ cd robobotrpgweb-client
+ npm install
+ npm run dev
+ cd ..
+
+ echo "Setting up backend..."
+ cd RobobotRPG-api
+ npm install
+ cd ..
+
+ echo "Starting server..."
+ cd RobobotRPG-api
+ npm run dev
