d22ba2d2e3c87cf2168e0e74df71f2e129c80e3e
mclainv <rma9540@nyu.edu>
2025-04-30 02:58:19 -0400

    console log session id

--- robobotrpgweb-client/src/utils/helpers.ts
+++ robobotrpgweb-client/src/utils/helpers.ts
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext } from "next";
 
 export const validateCookies = (ctx: GetServerSidePropsContext) => {
     const sessionID = ctx.req.cookies['connect.sid'];
+    console.log("sessionID is ", sessionID);
     return sessionID ? ({
         Cookie: `connect.sid=${sessionID}`
     }) : false;
